cda11d1dca4cd52b728ec7a6c54a6fa8b7a4714a
KiminatorGH <90854413+KiminatorGH@users.noreply.github.com>
2025-10-24 17:06:16 -0400

    Update validateNumerical.js

--- libraries/js/validateNumerical.js
+++ libraries/js/validateNumerical.js
@@ -1,1 +1,28 @@
+/****************************************************************************************
+* This function validates if a provide variable x is a number between upperLimit and 
+* lowerLimit.
+* 
+* Parameters (any parameters denoted in this document within [] are optional)
+*  - x
+*  - [lowerLimit]=0x8000000000000000
+*  - [upperLimit]=0xFFFFFFFFFFFFFFFF
+*  
+* Returns: boolean true if x is numerical and lowerLimit <= x >= upperLimiit
+*
+* Usage: 
+*        x = any input from user or any result from other fuction;
+*        isNumber=validateNumerical(x);
+*        or
+*        isNumber=validateNumerical(x, -100, +100);
+*
+* Version: 1.0
+* Author: O. XYZ
+* Date: Oct. 24, 2025
+* Reviewed by: R. XYZ
+* Approved by: A. XYZ
+*****************************************************************************************/
 
+function validateNumerical(x, lowerLimit=0x8000000000000000, upperLimit=0xFFFFFFFFFFFFFFFF)
+  {
+  return (typeof(x)==='number' && (x>=lowerLimit && x<=upperLimit));
+  }
