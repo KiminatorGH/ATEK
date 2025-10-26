6cff5c469a00e46b83deda9a2501e7e25a73fa5f
KiminatorGH <90854413+KiminatorGH@users.noreply.github.com>
2025-10-26 12:12:51 -0400

    Update formatAPIOutput.php

--- libraries/php/formatAPIOutput.php
+++ libraries/php/formatAPIOutput.php
@@ -1,1 +1,27 @@
+/*
+This function format $obj into an OpenAPI 3.x compliant JSON........
 
+Parameters: $obj - contains data that needs to be returned to the API client.
+
+Returns: $resJSON that contains the data in $obj and all the required fiels in compliance to OpenAPI 3.x
+
+Usage: $json=formatAPIOutput($obj);
+
+Author: .....
+Date: .....
+Reviewed by: ....
+Review date: ....
+Approved by: ....
+Approve date: ....
+Version: ....
+
+*/
+
+function formatAPIOutput($obj)
+  {
+  /*
+  Codes to verify if all required OpenAPI 3.x fields are in the $obj.
+  Create a final object ($finalObj) that contains all the required fields + data in $obj
+  */
+  return (json_encode($finalObj);
+  }
