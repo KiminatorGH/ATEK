c3bfcafbcbe1a22772181d7a8f8ff2a072589d83
KiminatorGH <90854413+KiminatorGH@users.noreply.github.com>
2025-10-26 12:23:59 -0400

    Update simpleAPI_01.php

--- src/API_01/simpleAPI_01.php
+++ src/API_01/simpleAPI_01.php
@@ -1,1 +1,24 @@
+/*
+This is an example of an API application to demonstrate the code structure that is scalable and adaptable.
+It does not contain the complete coding.
+It represents one of the API endpoint
+
+For this example, it is assuming that the calling client is using POST.
+
+Parameters: userId - string user's Id,
+            userSecret - string user's secret
+
+Usage: [from a jquery]: $.post("simpleAPI_01.php", {userId: "xxxxx", userSecret: "dksaskhasdf"}, function(resp)
+        {
+        // Do something with resp//
+        });
+
+Author: .....
+Date: ...
+Reviewed by: ....
+Review date: ....
+Approved by: ....
+Approved date: ....
+Revision: ....
+*/
 
